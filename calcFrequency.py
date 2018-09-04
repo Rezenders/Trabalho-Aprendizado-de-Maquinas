a00e0e4feb111f2dbf738c202510a5435fc81f56
thiago.thalison <thalisonfirmino@gmail.com>
2018-09-03 23:32:21 -0300

    Add comments on calcFrequency.py

--- calcFrequency.py
+++ calcFrequency.py
@@ -8,7 +8,9 @@ from nltk.stem.porter import PorterStemmer
 path = 'test.txt'
 token_dict = {}
 
-
+# Funcao que faz a quebra do texto em palavras para posterior contagem
+# Ela e passada para a biblioteca TfidfVectorizer.
+# Notem que ela tambem ja faz o processo de stemming. 
 def tokenize(text):
     tokens = nltk.word_tokenize(text)
     stems = []
@@ -16,19 +18,23 @@ def tokenize(text):
         stems.append(PorterStemmer().stem(item))
     return stems
 
-
+# Aqui abre o arquivo, coloca o texto em lower case e elimina pontuacoes.
 with open('test.txt') as pearl:
     text = pearl.read()
     token_dict['test'] = text.lower().translate(None, string.punctuation)
 
+# Calcula o TF*IDF com base no documento test.txt
 tfidf = TfidfVectorizer(tokenizer=tokenize, stop_words='english')
-tfs = tfidf.fit_transform(token_dict.values())
+tfidf_matrix = tfidf.fit_transform(token_dict.values())
 
-# Uma vez computado o Idf, a gente pode calcular o TF*IDF para cada documento que a gente for usar para treino
-# Assim como eu fiz com esta string de exemplo.
-textCalculateTFIDF = 'My research academic editor chair and academic.'
-response = tfidf.transform([textCalculateTFIDF])
-
+# Recupera o nome dos tokes utilizados no calculo do TF*IDF
 feature_names = tfidf.get_feature_names()
-for col in response.nonzero()[1]:
-    print feature_names[col], ' - ', response[0, col]
+
+# Recupera o score TF*IDF do documento utilizado no calculo
+doc = 0
+feature_index = tfidf_matrix[doc,:].nonzero()[1]
+tfidf_scores = zip(feature_index, [tfidf_matrix[doc, x] for x in feature_index])
+
+# Faz um print dos tokens e TF*IDF calculados
+for w, s in [(feature_names[i], s) for (i, s) in tfidf_scores]:
+  print w, s
