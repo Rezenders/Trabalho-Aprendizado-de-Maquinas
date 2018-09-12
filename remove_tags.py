b88ec1c9b01dd1fc002d3b01ffeab7e32f75538a
Gustavo Rezende <gustavorezendesilva@hotmail.com>
2018-09-12 14:53:27 -0300

    ✨ Removendo tudo antes do html

--- remove_tags.py
+++ remove_tags.py
@@ -4,14 +4,15 @@ from pathlib import Path
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-#cleanr = re.compile('<.*?>') #Alternative regular expression
-cleanr = re.compile(r'<[^>]+>')
-
 # Removes all the html tags contained in the string passed
 # @param raw_html string with a full html file, with all the tags
 # @returns string without html tags
 def remove_html_tags(raw_html):
-    cleantext = re.sub(cleanr, '', raw_html)
+    clean_headers = re.compile('^[^<]+', flags=re.DOTALL)
+    no_headers = re.sub(clean_headers, '', raw_html)
+
+    cleanr = re.compile(r'<[^>]+>',flags=re.IGNORECASE)
+    cleantext = re.sub(cleanr, '', no_headers)
     return cleantext
 
 # Removes all the html tags contained in the html file passed
@@ -27,22 +28,20 @@ def remove_tags_from_file(file_path):
 def remove_stop_words(clean_html):
     stop_words = set(stopwords.words('english'))
 
-    # Add words that are not useful to the set
+    # Add words that are not usefull to the set
     stop_words.update(['MIME-Version','text/html','Content-Length','Content-Type'])
-    
+
     word_tokens = word_tokenize(clean_html)
-    
     clean_stop_words = [w for w in word_tokens if not w in stop_words]
-    
+
     return clean_stop_words
 
 def html_pre_processing(html_path):
     with open(Path(html_path)) as html_file:
         clean_html = remove_html_tags(html_file.read())
-        
         clean_stop_words = remove_stop_words(clean_html)
         print(clean_stop_words)
-        
+
 
 
 ## The following code was used for testing and will not be used in the final code
