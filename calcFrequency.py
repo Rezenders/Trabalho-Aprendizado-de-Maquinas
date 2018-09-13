dfa884c8072d34173d6a148a019c0a52f24e1e96
thiago.thalison <thalisonfirmino@gmail.com>
2018-09-12 23:18:23 -0300

    Add trainning and testing data sets

--- calcFrequency.py
+++ calcFrequency.py
@@ -13,6 +13,28 @@ from operator import itemgetter
 from itertools import islice
 from collections import defaultdict
 
+import csv
+
+# Estes valores sao baseados no uso somente
+# dos arquivos na pasta classifierSets\trainning
+course_trainning_set = range(0, 649)
+department_trainning_set = range(649, 777)
+faculty_trainning_set = range(777, 1561)
+other_trainning_set = range(1561, 4194)
+project_trainning_set = range(4194, 4545)
+staff_trainning_set = range(4545, 4639)
+student_trainning_set = range(4639, 5786)
+
+# Estes valores sao baseados no uso somente
+# dos arquivos na pasta classifierSets\test
+course_testing_set = range(0, 281)
+department_testing_set = range(281, 335)
+faculty_testing_set = range(335, 675)
+other_testing_set = range(675, 1806)
+project_testing_set = range(1806, 1959)
+staff_testing_set = range(1959, 2002)
+student_testing_set = range(2002, 2496)
+
 # Funcao que faz a quebra do texto em palavras para posterior contagem
 # Ela e passada para a biblioteca TfidfVectorizer.
 # Notem que ela tambem ja faz o processo de stemming.
@@ -40,8 +62,8 @@ def token_dict_from_dir(trainning_path):
     for dirpath, dirs, files in os.walk(trainning_path):
         for f in files:
             fname = os.path.join(dirpath, f)
-            counter = counter + 1
             token_dict[counter] = token_from_file(fname)
+            counter = counter + 1
 
     return token_dict
 
@@ -103,49 +125,158 @@ def select_best_features(tfidf_matrix, feature_names, classBounds):
 
     best_features_dict = OrderedDict(sorted(best_features_dict.items(), key=itemgetter(1), reverse=True))
     best_features_dict = OrderedDict(islice(best_features_dict.iteritems(),0,50))
-    print best_features_dict
+    return best_features_dict.keys()
+
+def calculateWordFrequency(features_list, tokens):
+    frequencies = []
+    for feature in features_list:
+        frequency = tokens.count(feature)
+        frequencies.append(frequency)
+    return frequencies
+
+def resolveTrainningCandidateClass(key):
+    if key in course_trainning_set:
+        return "course"
+    elif key in department_trainning_set:
+        return "department"
+    elif key in faculty_trainning_set:
+        return "faculty"
+    elif key in other_trainning_set:
+        return "other"
+    elif key in project_trainning_set:
+        return "project"
+    elif key in staff_trainning_set:
+        return "staff"
+    elif key in student_trainning_set:
+        return "student"
+    else:
+        return "unkown"
+
+def resolveTestingCandidateClass(key):
+    if key in course_testing_set:
+        return "course"
+    elif key in department_testing_set:
+        return "department"
+    elif key in faculty_testing_set:
+        return "faculty"
+    elif key in other_testing_set:
+        return "other"
+    elif key in project_testing_set:
+        return "project"
+    elif key in staff_testing_set:
+        return "staff"
+    elif key in student_testing_set:
+        return "student"
+    else:
+        return "unkown"
+
+def generateTrainningSet(features_list):
+    trainning_path = "./classifierSets/trainning"
+    token_dict = token_dict_from_dir(trainning_path)
+
+    trainning_set = []
+    features_list.append("class")
+    trainning_set.append(features_list)
+    
+    for key, value in token_dict.iteritems():
+        tokens = tokenize(value)
+        frequencies = calculateWordFrequency(features_list, tokens)
+        frequencies.append(resolveTrainningCandidateClass(key))
+        trainning_set.append(list(frequencies))
+
+    return trainning_set
+
+def generateTestingSet(features_list):
+    test_path = "./classifierSets/test"
+    token_dict = token_dict_from_dir(test_path)
+    
+    testing_set = []
+    features_list.append("class")
+    testing_set.append(features_list)
+
+    for key, value in token_dict.iteritems():
+        tokens = tokenize(value)
+        frequencies = calculateWordFrequency(features_list, tokens)
+        frequencies.append(resolveTestingCandidateClass(key))
+        testing_set.append(list(frequencies))
+    
+    return testing_set
+
+
+def generateCsv(fileName, data):
+    csv_file = open(fileName, "w")
+    writer = csv.writer(csv_file, lineterminator='\n')
+
+    for line in data:
+        writer.writerow(line)
 
 
 def main():
     trainning_path = "./classifierSets/trainning"
     token_dict = token_dict_from_dir(trainning_path)
 
-    # Calcula o TF*IDF com base no documento test.txt
+    # Calcula o TF*IDF
     tfidf = TfidfVectorizer(tokenizer=tokenize, stop_words='english')
     tfidf_matrix = tfidf.fit_transform(token_dict.values())
 
     # Recupera o nome dos tokes utilizados no calculo do TF*IDF
     feature_names = tfidf.get_feature_names()
 
-
     # Calcula lista de features por classe.
     print "Course"
-    select_best_features(tfidf_matrix, feature_names, range(0, 649))
+    best_features_course = select_best_features(tfidf_matrix, feature_names, course_trainning_set)
+    print best_features_course
     print ""
 
     print "Department"
-    select_best_features(tfidf_matrix, feature_names, range(649, 777))
+    best_features_department = select_best_features(tfidf_matrix, feature_names, department_trainning_set)
+    print best_features_department
     print ""
 
     print "Faculty"
-    select_best_features(tfidf_matrix, feature_names, range(777, 1561))
+    best_features_faculty = select_best_features(tfidf_matrix, feature_names, faculty_trainning_set)
+    print best_features_faculty
     print ""
 
     print "Other"
-    select_best_features(tfidf_matrix, feature_names, range(1581, 4194))
+    best_features_other = select_best_features(tfidf_matrix, feature_names, other_trainning_set)
+    print best_features_other
     print ""
 
     print "Project"
-    select_best_features(tfidf_matrix, feature_names, range(4194, 4545))
+    best_features_project = select_best_features(tfidf_matrix, feature_names, project_trainning_set)
+    print best_features_project
     print ""
 
     print "Staff"
-    select_best_features(tfidf_matrix, feature_names, range(4545, 4639))
+    best_features_staff = select_best_features(tfidf_matrix, feature_names, staff_trainning_set)
+    print best_features_staff
     print ""
 
     print "Student"
-    select_best_features(tfidf_matrix, feature_names, range(4639, 5786))
+    best_features_student = select_best_features(tfidf_matrix, feature_names, student_trainning_set)
+    print best_features_student
     print ""
+
+    #Cria um set com todas as features extraidas
+    features_set = set()
+    features_set.update(best_features_course)
+    features_set.update(best_features_department)
+    features_set.update(best_features_faculty)
+    features_set.update(best_features_other)
+    features_set.update(best_features_project)
+    features_set.update(best_features_staff)
+    features_set.update(best_features_student)
+
+    print "Total de Features: ", len(features_set)
+
+    #Cria um CSV com os dados para treino
+    trainning_set = generateTrainningSet(list(features_set))
+    generateCsv("trainningSet.csv", trainning_set)
+
+    #Cria um CSV com os dados de test
+    testing_set = generateTestingSet(list(features_set))
+    generateCsv("testingSet.csv", testing_set)
 
 if __name__ == "__main__":
     main()
